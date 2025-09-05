ddbb4c99bfd6dc7f37431fc725a5a75fdd2b7688
NikoTeLp90 <nikote.lp@gmail.com>
2025-09-05 20:44:10 -0300

    .

--- Prueba/pruebaa/pruebaa/settings.py
+++ Prueba/pruebaa/pruebaa/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kh**7jopvx=2go0rqeum5#!&ob#wbdzjatrq*km!$#m6i26t_q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vercell.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
