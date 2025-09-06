5a9c52130dd4802c2388a0631f0384dd1584fca1
NikoTeLp90 <nikote.lp@gmail.com>
2025-09-06 11:52:45 -0300

    fix

--- pruebaa/crud/views.py
+++ pruebaa/crud/views.py
@@ -5,5 +5,4 @@ from django.views.generic import ListView, CreateView, UpdateView, DeleteView, T
 
 
 class HomeView(TemplateView):
-    template_name = 'home.html'
-    
+    template_name = 'crud/home.html'
