d5046f8ce7466b2b94d6fd27f9c4f12294b55749
NikoTeLp90 <nikote.lp@gmail.com>
2025-09-06 11:23:57 -0300

    mas fixes...

--- pruebaa/api/index.py
+++ pruebaa/api/index.py
@@ -1,4 +1,6 @@
-from pruebaa.asgi import application
+from django.core.asgi import get_asgi_application
 from mangum import Mangum
 
+application = get_asgi_application()
 handler = Mangum(application)
+
