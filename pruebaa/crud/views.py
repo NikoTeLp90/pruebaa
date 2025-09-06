eefcce05a95c8f666988fdc6aab02c990aba92a3
NikoTeLp90 <nikote.lp@gmail.com>
2025-09-06 11:50:39 -0300

    dont know the urls.py xD

--- pruebaa/crud/views.py
+++ pruebaa/crud/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, TemplateView, DetailView
 
 # Create your views here.
+
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+    
